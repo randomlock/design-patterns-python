4045dc74446e1ce90ebf99dfc1efcbe620560400
abdullah <abdullah@webintensive.com>
2020-02-13 12:24:40 +0530

    updated state pattern

--- behavioral/state.py
+++ behavioral/state.py
@@ -1,25 +1,30 @@
 from abc import ABC, abstractmethod
-from collections import defaultdict
 
 """
 State Pattern
 
-- Provides a mechanism for publish-subscribe model
-- Very useful for event based programming where changes in source triggers different targets
-- Follow Open Close Relationship
-- Example - Database triggers. When you are creating/updating a record and you need to perform some
-  actions before/after it
+- State is a behavioral design pattern that lets an object alter its behavior when its internal
+state changes
+- Very useful for programs having finite state machine.
+- Follow Open Close Relationship and Single Responsibility principle
+- Example - Vending Machine. A vending machine can be in "Ready", "Dispensing Item", "Dispensing Change"
+"Transaction Rollback" state. Each state will determine what actions it can perform. So instead of using single
+class and using switch/if-else and cluttering everything in one place, we can use state pattern for state
+management.
 """
 
 
 """
 How to create
 
-1) Create a Manager that will handle subscriptions
-2) Create a publisher that will notify the manger about state change
-3) Create subscribers that implement a common interface. You can pass reference to publisher as 
-    different subscriber may need different data
-4) In the client code, add relevant subscriber to relevant event type.
+1) Create a Manager that will delegate all its work to a state class. This expose setter to change
+state
+2) Create a interface/base class that all state class will inherit
+3) Create different state class. You can also pass the manager object so that they get additional
+info from manager class
+4) The state class will do something and may be set new state. For this, it must have access to
+Manager class object. Or you can use client code or Manager class to change the state. To avoid
+any possibilities of setting new state, use only manager/state class to change to next state.  
 """
 
 
@@ -85,6 +90,7 @@ class DispenseItem(VendingMachineState):
     def cancel_transaction(self, vending_machine, item_id):
         item = vending_machine.get_item(item_id)
         vending_machine.remove_cash(item.price)
+        print('Transaction for item - {} is successfully cancelled'.format(item_id))
         vending_machine.set_state(Ready())
 
     def dispense_item(self, vending_machine, item_id):
@@ -95,6 +101,8 @@ class DispenseItem(VendingMachineState):
                 item_id,
             ))
             vending_machine.remove_item(item_id)
+        else:
+            print('The transaction is cancelled. Please order again')
 
 
 class VendingMachine:
@@ -137,13 +145,23 @@ if __name__ == '__main__':
     vending_machine.add_item(1, 'Chips', 20)
     vending_machine.add_item(2, 'Chocolate', 30)
     item_id = 1
+
+    vending_machine.collect_cash(20, item_id)
+    vending_machine.cancel_transaction(item_id)
+    vending_machine.dispense_item(item_id)
+
     vending_machine.collect_cash(20, item_id)
     vending_machine.dispense_item(item_id)
 
 """
 Output
 
-Data table_1 is created in table hello. You can do something here
-Data table_1 is created in table bye. You can do something here
-Alert: Data table_1 was deleted from table bye
+The cash given 20$ matches item Chips price 20$. Proceeding further
+Transaction for item - 1 is successfully cancelled
+NotImplementedError: Cannot dispense item before receiving cash
+
+
+The cash given 20$ matches item Chips price 20$. Proceeding further
+Item Chips with id 1 is available. Giving it to customer
+
 """
